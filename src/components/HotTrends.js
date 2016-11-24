3e27c14bd4382e1fefacd78a65296973b9853584
codiechanel <codiechanel@gmail.com>
2016-11-24 22:33:43 +0800

    cool

--- src/components/HotTrends.js
+++ src/components/HotTrends.js
@@ -9,56 +9,63 @@ import 'rxjs/add/observable/dom/ajax'
 export default class HotTrends extends Component {
 
   constructor(props) {
-        super(props);
-        this.state = { items: [] }
-        this.props = props
-     //   this.handler = this.handler.bind(this)
-    }
+    super(props);
+    this.state = { items: [] }
+    this.props = props
+    this.rows = this.rows.bind(this)
+  }
+
+  createMarkup(text) {
+    return { __html: text };
+  }
 
   rows(item, index) {
-    let targetLink = `/facebookList/${index}`
+    let targetLink = `/facebookLis/${index}`
 
     return (<div className="list-group-item" key={index}>
-      <Link to={targetLink}><input readOnly style={{ border: 0 }} value={item.title} />  </Link>
+      <Link to={targetLink}><b> {item.title} </b>  </Link>
+     <div dangerouslySetInnerHTML={this.createMarkup(item["ht:news_item"]["0"]["ht:news_item_title"][0])} />
+  
     </div>)
 
   }
 
   render() {
-     return (<div style={divStyle} >
+    return (<div style={divStyle} >
 
-            <h1 style={{ padding: '5px' }}>Categories</h1>
-            <div style={{ flex: 1 }} className="list-group">
-                {this.state.items.map(this.rows)}
-            </div>
-        
+      <h1 style={{ padding: '5px' }}>Categories</h1>
+      <div style={{ flex: 1 }} className="list-group">
+        {this.state.items.map(this.rows)}
+      </div>
 
-        </div>)
+
+    </div>)
   }
 
   componentDidMount() {
-     let url = 'http://1de8a0b2-0ee0-4-231-b9ee.azurewebsites.net/hotTrendsDetail'
+    let url = 'http://1de8a0b2-0ee0-4-231-b9ee.azurewebsites.net/hotTrendsDetail'
     const settings2 = {
       url,
       responseType: 'json'
     }
 
-     Rx.Observable.ajax(settings2).subscribe(e => {
-       this.setState({items : e.response.rss.channel[0].item })
-      
-       console.log( e.response.rss.channel[0].item, e) })
+    Rx.Observable.ajax(settings2).subscribe(e => {
+      this.setState({ items: e.response.rss.channel[0].item })
+
+      console.log(e.response.rss.channel[0].item, e)
+    })
 
   }
 
 
 }
 
- const divStyle = {
+const divStyle = {
 
-            display: 'flex',
-            // height: '100%',
-            flex: 1,
-            flexDirection: 'column',
-            backgroundColor: '#657687'
+  display: 'flex',
+  // height: '100%',
+  flex: 1,
+  flexDirection: 'column',
+  backgroundColor: '#657687'
 
-        }
+}
