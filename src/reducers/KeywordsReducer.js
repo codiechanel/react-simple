a5cb0e59d2fc2081a7011421256c9ed93afd2078
codiechanel <codiechanel@gmail.com>
2016-11-27 00:50:00 +0800

    update wrkin

--- src/reducers/KeywordsReducer.js
+++ src/reducers/KeywordsReducer.js
@@ -3,16 +3,16 @@ import * as constant from '../common/constants'
 function KeywordsReducer(state = [], action) {
   switch (action.type) {
     case constant.KEYWORDS_LOADED:
-       return action.payload
+      return action.payload
     case constant.KEYWORD_ADDED:
       return [...state, action.payload]
-     //  return Object.assign({}, state, {  action: action.type })
+    //  return Object.assign({}, state, {  action: action.type })
     case constant.KEYWORD_DELETED:
-   
+
       return state.filter(item => item.objectId !== action.objectId)
-      case constant.KEYWORD_UPDATED:
-       console.log('keyword updated')
-      return state
+    case constant.KEYWORD_UPDATED:
+      let filtered = state.filter(item => item.objectId !== action.payload.objectId)
+      return [...filtered, action.payload]
 
     default:
       return state
